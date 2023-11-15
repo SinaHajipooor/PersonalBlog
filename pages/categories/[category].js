b43792e2882167ffabfd132c4b6e791baadb1a58
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-11-15 12:03:06 +0330

    style post card

--- pages/categories/[category].js
+++ pages/categories/[category].js
@@ -9,60 +9,60 @@ import { slugify } from "@lib/utils/textConverter";
 const { blog_folder } = config.settings;
 
 const Category = ({ posts, slug }) => {
-  return (
-    <Base>
-      <div className="section">
-        <div className="container">
-          <div className="row">
-            <div className="mx-auto lg:col-10">
-              <h1 className="text-center capitalize">{slug}</h1>
-              <div className="row pt-12">
-                {posts.map((post, i) => (
-                  <Post
-                    className="mb-6 sm:col-6"
-                    key={"key-" + i}
-                    post={post}
-                  />
-                ))}
-              </div>
+    return (
+        <Base>
+            <div className="section">
+                <div className="container">
+                    <div className="row">
+                        <div className="mx-auto lg:col-10">
+                            <h1 className="text-center capitalize">{slug}</h1>
+                            <div className="row pt-12">
+                                {posts.map((post, i) => (
+                                    <Post
+                                        className="mb-6 sm:col-6"
+                                        key={"key-" + i}
+                                        post={post}
+                                    />
+                                ))}
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </Base>
-  );
+        </Base>
+    );
 };
 
 export default Category;
 
 export const getStaticPaths = () => {
-  const allCategory = getTaxonomy(`content/${blog_folder}`, "categories");
-  const paths = allCategory.map((category) => ({
-    params: {
-      category: category,
-    },
-  }));
+    const allCategory = getTaxonomy(`content/${blog_folder}`, "categories");
+    const paths = allCategory.map((category) => ({
+        params: {
+            category: category,
+        },
+    }));
 
-  return {
-    paths,
-    fallback: false,
-  };
+    return {
+        paths,
+        fallback: false,
+    };
 };
 
 export const getStaticProps = ({ params }) => {
-  const posts = getSinglePage(`content/${blog_folder}`);
-  const filteredPosts = posts.filter((post) =>
-    post.frontmatter.categories.find((category) =>
-      slugify(category).includes(params.category)
-    )
-  );
+    const posts = getSinglePage(`content/${blog_folder}`);
+    const filteredPosts = posts.filter((post) =>
+        post.frontmatter.categories.find((category) =>
+            slugify(category).includes(params.category)
+        )
+    );
 
-  const sortedPosts = sortByDate(filteredPosts);
+    const sortedPosts = sortByDate(filteredPosts);
 
-  return {
-    props: {
-      posts: filteredPosts,
-      slug: params.category,
-    },
-  };
+    return {
+        props: {
+            posts: filteredPosts,
+            slug: params.category,
+        },
+    };
 };
